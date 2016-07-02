e3cc41a8634c9c5546a4d13d52c65fd42c1caf40
Prabhakar Gupta <prabhakar.gupta@nsitonline.in>
2016-07-02 19:52:10 +0530

    version 0.1.0.1

--- setup.py
+++ setup.py
@@ -2,20 +2,21 @@
 # @Author: prabhakar
 # @Date:   2016-06-28 23:44:47
 # @Last Modified by:   Prabhakar Gupta
-# @Last Modified time: 2016-07-01 00:41:45
+# @Last Modified time: 2016-07-02 19:51:32
 #!/usr/bin/env python
 
 from setuptools import setup, find_packages
 
 setup(
     name='website-status',
-    version='0.1.0.0',
-    description='',
+    version='0.1.0.1',
+    description='Get website status of any URL',
+    long_description='Check the status of any website i.e., if it is up or not using command line',
     author='Prabhakar Gupta',
     license='MIT',
-    keywords="",
-    author_email='',
-    url='',
+    keywords="website-status website status status_code code",
+    author_email='prabhakargupta267@gmail.com',
+    url='https://github.com/prabhakar267/website-status-cli',
     packages=find_packages(),
     install_requires=[
         "click",
